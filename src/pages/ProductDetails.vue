2b1ff35798237641a4a988377120baea80f33377
Francis Badasu <nytro04@gmail.com>
2021-10-27 14:30:53 +0000

    updates: product details redo

--- src/pages/ProductDetails.vue
+++ src/pages/ProductDetails.vue
@@ -1,84 +1,37 @@
 <template>
-	<div class="bg-white">
-		<div class="pt-6">
-			<div
-				class="max-w-2xl mx-auto mt-6 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8"
-			>
-				<div
-					class="hidden overflow-hidden rounded-lg aspect-w-3 aspect-h-4 lg:block"
-				>
-					<img
-						:src="product.image"
-						:alt="product.title"
-						class="object-cover object-center w-full h-full"
-					/>
+	<div class="max-w-2xl mx-auto mt-6 bg-white sm:px-6 lg:max-w-7xl lg:px-8">
+		<div class="pt-20">
+			<h2 class="mb-20 text-2xl font-extrabold tracking-tight text-gray-900">
+				Products Details
+			</h2>
+			<div class="lg:grid lg:grid-cols-2 lg:gap-x-16">
+				<div>
+					<img :src="product.image" :alt="product.title" class="w-2/3" />
 				</div>
-				<div class="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
-					<div class="overflow-hidden rounded-lg aspect-w-3 aspect-h-2">
-						<div
-							class="py-10 lg:pt-6 lg:pb-16 lg:col-start-1 lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8"
-						>
-							<!-- Description and details -->
-							<div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-								<h1
-									class="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl"
-								>
-									{{ product.title }}
-								</h1>
-							</div>
-							<div>
-								<h3 class="sr-only">Description</h3>
-
-								<div class="space-y-6">
-									<p class="text-base text-gray-900">
-										{{ product.description }}
-									</p>
-								</div>
-							</div>
-
-							<div class="mt-10">
-								<h2 class="text-sm font-medium text-gray-900">Details</h2>
-
-								<div class="mt-4 space-y-6">
-									<p class="text-sm text-gray-600">{{ product.details }}</p>
-								</div>
-							</div>
-						</div>
+				<div class="space-y-10">
+					<h3 class="text-xl font-bold text-gray-900">
+						{{ product.title }}
+					</h3>
+					<div>
+						<h3 class="mb-4 font-medium text-gray-900">Description</h3>
+						<p class="text-base text-gray-600">
+							{{ product.description }}
+						</p>
 					</div>
-				</div>
-				<div
-					class=" aspect-w-4 aspect-h-5 sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4"
-				>
-					<div class="mt-4 lg:mt-0 lg:row-span-3">
-						<h2 class="sr-only">Product information</h2>
-						<p class="text-3xl text-gray-900">{{ product.price }}</p>
+					<div class="flex items-center justify-between">
+<div class="">
+	<h3 class="mb-4 font-medium text-gray-900">Price</h3>
 
-						<!-- Reviews -->
-						<div class="mt-6">
-							<h3 class="sr-only">Reviews</h3>
-							<div class="flex items-center">
-								<div class="flex items-center">
-									<!-- stars  -->
-									<!-- <StarIcon
-										v-for="rating in [0, 1, 2, 3, 4]"
-										:key="rating"
-										:class="[
-											reviews.average > rating
-												? 'text-gray-900'
-												: 'text-gray-200',
-											'h-5 w-5 flex-shrink-0',
-										]"
-										aria-hidden="true"
-									/> -->
-								</div>
-								<!-- <p class="sr-only">{{ reviews.average }} out of 5 stars</p> -->
-								<!-- <a
-									:href="reviews.href"
-									class="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500"
-									>{{ reviews.totalCount }} reviews</a
-								>
-							</div> -->
-							</div>
+						<h3 class="mb-4 text-3xl font-bold text-gray-900">{{ product.price }}</h3>
+</div>
+
+						<div>
+						<h3 class="mb-4 font-medium text-gray-900">Category</h3>
+						<p class="text-base text-gray-600">
+							{{ product.category }}
+							{{ product.rating.count }}
+							{{ product.rating.rate }}
+						</p>
 						</div>
 					</div>
 				</div>
@@ -98,11 +51,8 @@
 		props: {
 			productId: {
 				type: Number,
-				default: 0
-			}
-			// product: {
-			// 	type: Object as PropType<ProductType>,
-			// },
+				default: 0,
+			},
 		},
 		setup(props) {
 			const isLoading = ref(false)
@@ -125,14 +75,11 @@
 			})
 
 			const productBlock = async (): Promise<void> => {
-
 				isLoading.value = true
 
 				const productId = route.params.id
-				console.log(productId, 'product id')
 
 				data.product = await getProduct(productId)
-
 			}
 
 			productBlock()
